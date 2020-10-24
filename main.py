fd393a496e96d8172200db77df4312f05ea13b37
hong-il <clroyan01@gmail.com>
2020-10-24 15:31:55 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pymongo
+
+member = {
+    "name": "hong-il",
+    "age": 28,
+    "address": "seoul",
+    "profile": [
+        "a.jpg",
+        "b.jpg"
+    ]
+}
+
+member2 = {
+    "name": "hong-il",
+    "address": "seoul",
+    "profile": [
+        "a.jpg",
+        "b.jpg"
+    ]
+}
+
+conn = pymongo.MongoClient("localhost", 27017)
+db = conn.test  # Database
+col = db.members    # Collection
+
+col.insert_one(member)
+col.insert_one(member2)
