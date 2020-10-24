6019485be5e10c7606d3a011044f1058b1566845
hong-il <clroyan01@gmail.com>
2020-10-24 15:45:59 +0900

    MongoDB - NoSQL practice

--- main.py
+++ main.py
@@ -23,5 +23,12 @@ conn = pymongo.MongoClient("localhost", 27017)
 db = conn.test  # Database
 col = db.members    # Collection
 
-col.insert_one(member)
-col.insert_one(member2)
+# col.insert_one(member)
+
+# results = col.find({"$or": [{"age": 28}, {"address": "seoul"}]})
+# for r in results:
+#    print(r)
+
+results = col.find({"age": {"$gte": 28, "$lt": 29}})
+for r in results:
+    print(r)
